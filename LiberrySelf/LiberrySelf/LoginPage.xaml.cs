220c54b63c4b6e6d2fb44b3e52a942267efe1fb7
Jorno Havani <162573539+DwarfMorok@users.noreply.github.com>
2025-04-10 17:25:21 +0300

    Add files via upload

---
+++ LiberrySelf/LiberrySelf/LoginPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LiberrySelf
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        private string captchaCode;
+        private int failAttempts = 0;
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            GenerateCaptcha();
+        }
+
+        // Генерация капчи
+        private void GenerateCaptcha()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var rand = new Random();
+            captchaCode = new string(Enumerable.Range(0, 6).Select(x => chars[rand.Next(chars.Length)]).ToArray());
+            CaptchaText.Text = captchaCode;
+        }
+
+        // Кнопка "Войти"
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            using var db = new LibraryBD();
+            var user = db.Librarians.FirstOrDefault(l => l.Login == Login.Text && l.Password == Password.Password);
+
+            if (user != null)
+            {
+                new LibrarianPage().Show();
+                Close();
+            }
+            else
+            {
+                failAttempts++;
+                MessageBox.Show("Неверный логин или пароль");
+                if (failAttempts >= 1 && CaptchaInput.Text != captchaCode)
+                {
+                    MessageBox.Show("Капча введена неверно");
+                    GenerateCaptcha();
+                }
+            }
+        }
+    }
---
+++ LiberrySelf/LiberrySelf/RentPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LiberrySelf
+{
+    /// <summary>
+    /// Логика взаимодействия для RentPage.xaml
+    /// </summary>
+    public partial class RentPage : Window
+    {
+        public RentPage()
+        {
+            InitializeComponent();
+        }
+
+        // Проверка ввода всех данных
+        private void CheckInputs() => RentButton.IsEnabled =
+            !string.IsNullOrWhiteSpace(BookId.Text) &&
+            !string.IsNullOrWhiteSpace(Name.Text) &&
+            !string.IsNullOrWhiteSpace(Phone.Text) &&
+            ReturnDate.SelectedDate != null;
+
+        // Кнопка "Арендовать"
+        private void RentButton_Click(object sender, RoutedEventArgs e)
+        {
+            using var db = new LibraryBD();
+            db.Rents.Add(new Rent
+            {
+                BookId = int.Parse(BookId.Text),
+                FullName = Name.Text,
+                Phone = Phone.Text,
+                DueDate = ReturnDate.SelectedDate.Value
+            });
+            db.SaveChanges();
+            Close();
+        }
+    }
